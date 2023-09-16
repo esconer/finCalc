0e6750743fbc6f73b8b407db0d585bfbe34005ee
esconer <hikigaya07@proton.me>
2023-09-16 21:04:46 +0530

    function added to choose UCF & NUCF

--- finmanage/capital budgeting/payback_period.py
+++ finmanage/capital budgeting/payback_period.py
@@ -2,6 +2,8 @@
 # where annual cash inflow = profit after tax + depreciation
 #where profit after tax = profit before tax - tax  or profit before tax(1- tax rate)
 
+
+#situation 1: uniform cashflow throughout the years
 # one project
 
 initial = int(input("what is your initial investment?\n--> "))
@@ -10,5 +12,15 @@ dep=int(input('what is your annual depreciation?\n'))
 tax_rate= int(input('what is the tax rate?\n (e.g. 40 it will mean 40%)\n--> '))
 profit_a_tax= profit_b_tax*(1-(tax_rate/100))
 annual_cashinflow=profit_a_tax-dep
-payback_period= round(initial/annual_cashinflow,2)
-print(f"your initial investment will be recovered within {payback_period} years")
+# payback_period= round(initial/annual_cashinflow,2)
+# print(f"your initial investment will be recovered within {payback_period} years")
+
+
+#non-uniform cashflow
+
+years =int(input("How many years of cash flows?\n"))
+cashflows =[]
+for i in range(years):
+   cashflow = float(input(f"Cash flow of year {i + 1}: "))  
+   cashflows.append(cashflow) 
+
